6ec1b20734fc4cddddd5b783f575a606ab2daaf5
andres.montoya <andres.montoya@iesonline.com.co>
2021-07-30 14:27:46 -0500

    feat: configuracion de la prueba de carga

---
+++ locust/split_request.py
@@ -0,0 +1,38 @@
+from locust import task, between
+from locust.contrib.fasthttp import FastHttpUser
+from locust import events
+
+limit = 10
+cont = 0
+
+
+class WebsiteUser(FastHttpUser):
+
+    def increseCount(self):
+        global cont
+        cont += 1
+
+    def isNotLimitCount(self):
+        global cont
+        global limit
+        return cont <= limit
+
+    @task
+    def search_all2(self):
+        self.increseCount()
+        if self.isNotLimitCount():
+            response_request = self.client.post("/resquest", name="01 /resquest")
+
+            while True:
+                response_result = self.client.post("/result", json={
+
+                    "value": response_request.text
+                }, name="02 /result")
+
+                # print(response.text)
+
+                if response_result.text == 'true':
+                    break
+
+        else:
+            self.environment.runner.quit()
